8c6a422c78153ec8abd78db01fe9f20cf9c58b0d
najimattar <160213152+najimattar@users.noreply.github.com>
2026-03-01 16:55:29 +0530

    Update main.tf

--- terrform/main.tf
+++ terrform/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test_instance" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups = [var.Security_Group_ids]
+    vpc_security_group_ids = [var.Security_Group_ids]
     tags = {
         name = "test"
     }
