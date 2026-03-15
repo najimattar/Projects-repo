2da5454a3d72be907054097ee1a861ab8154be02
najimattar <160213152+najimattar@users.noreply.github.com>
2026-03-15 14:39:42 +0530

    Refactor to stop all EC2 instances dynamically
    Refactor stop_instance.py to dynamically stop all EC2 instances in the specified region instead of hardcoding instance IDs.

--- python/stop_instance.py
+++ python/stop_instance.py
@@ -1,15 +1,24 @@
 import boto3
 
-client = boto3.client('ec2', region_name='ap-south-1')
+# Create EC2 client
+ec2 = boto3.client('ec2')
 
-response = client.stop_instances(
-    InstanceIds=[
-        'i-058a7e2b76b1df5c1',
-        'i-0c37196ddeb481642',
-        'i-06c2da8f6205c00d5',
-        'i-0cef143ea60894b52'
-    ]
-)
+# Get all instances
+response = ec2.describe_instances()
 
-print("Instances stopped successfully")
-print(response)
+instance_ids = []
+
+# Collect all instance IDs
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_ids.append(instance['InstanceId'])
+
+# Stop instances
+if instance_ids:
+    ec2.stop_instances(InstanceIds=instance_ids)
+    
+    print("Stopping instances:")
+    for i in instance_ids:
+        print(i)
+else:
+    print("No instances found")
